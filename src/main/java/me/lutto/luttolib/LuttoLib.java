698e553e9f6a1a3a487470c63a1fe88e0610dfe0
StillLutto <iameinsteinhe@gmail.com>
2024-03-20 19:07:58 +0100

    Initial commit

---
+++ src/main/java/me/lutto/luttolib/LuttoLib.java
@@ -0,0 +1,17 @@
+package me.lutto.luttolib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LuttoLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
